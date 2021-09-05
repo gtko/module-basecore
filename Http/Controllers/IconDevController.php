8b9afb080f776612b136e1a5dfa86fdcfd10d18e
gtko <gtux.prog@gmail.com>
2021-09-05 22:00:20 +0200

    Update modules

--- Http/Controllers/IconDevController.php
+++ Http/Controllers/IconDevController.php
@@ -9,6 +9,10 @@ class IconDevController extends Controller
 {
     public function index()
     {
+        if(env('APP_ENV') !== 'local'){
+            abort(404);
+        }
+
         $icons = (new Icons())->getAll();
         return view('basecore::IconDev', compact('icons'));
     }
