6154e6845877f805c07298044203c5bb0b815995
gtko <gtux.prog@gmail.com>
2021-09-05 23:22:46 +0200

    Fix client formulaire

--- Resources/views/components/personne/address-details.blade.php
+++ Resources/views/components/personne/address-details.blade.php
@@ -1,13 +1,13 @@
 <div>
     <div class="flex flex-col justify-center items-center lg:items-start mt-4">
         <div class="truncate sm:whitespace-normal flex items-center mt-1">
-            <i data-feather="mail" class="w-4 h-4 mr-2"></i>{{ $personne->email }}
+            @icon('email', null, 'mr-2') {{ $personne->email }}
         </div>
         <div class="truncate sm:whitespace-normal flex items-center mt-1">
-            <i data-feather="phone" class="w-4 h-4 mr-2"></i> {{$personne->phone}}
+            @icon('phone', null, 'mr-2') {{$personne->phone}}
         </div>
         <div class="truncate sm:whitespace-normal flex items-start mt-1">
-            <i data-feather="home" class="w-4 h-4 mr-2"></i> {{$personne->fullAddress}}
+            @icon('home', null, 'mr-2') {{$personne->fullAddress}}
         </div>
     </div>
 </div>
