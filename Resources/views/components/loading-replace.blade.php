915e7656b3b0024e0903949272cc2233140e671d
gtko <gtko@users.noreply.github.com>
2021-11-16 12:35:57 +0100

    Améliorer loading-replace

--- Resources/views/components/loading-replace.blade.php
+++ Resources/views/components/loading-replace.blade.php
@@ -1,17 +1,22 @@
 @props([
     'label' => '',
     'size' => 20,
+    'loader' => null
 
 ])
 
 <div>
     <div wire:loading  {{$attributes}}>
-        <span class="flex justify-between items-center">
-            @icon('spinner', $size, 'animate-spin')
-            @if(!empty($label))
-                <span class="ml-2">{{$label}}</span>
-            @endif
-        </span>
+        @if($loader)
+            {{ $loader }}
+        @else
+            <span class="flex justify-between items-center">
+                @icon('spinner', $size, 'animate-spin')
+                    @if(!empty($label))
+                        <span class="ml-2">{{$label}}</span>
+                    @endif
+            </span>
+        @endif
     </div>
     <span wire:loading.remove>
         {{ $slot }}
