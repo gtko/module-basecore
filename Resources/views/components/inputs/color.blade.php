0c2a260305690aa3115befd5715ad60df5a6b5f0
gtko <gtko@users.noreply.github.com>
2021-11-19 01:46:32 +0100

    New pipeline (WIP)

--- Resources/views/components/inputs/color.blade.php
+++ Resources/views/components/inputs/color.blade.php
@@ -3,5 +3,20 @@
     'label',
     'value',
 ])
+@if($label ?? null)
+    @include('basecore::components.inputs.partials.label')
+@endif
 
-<x-basecore::inputs.basic type="color" :name="$name" label="{{ $label ?? ''}}" :value="$value ?? ''" :attributes="$attributes"></x-basecore::inputs.basic>
+<input
+    type="color"
+    id="{{ $name }}"
+    name="{{ $name }}"
+    value="{{ old($name, $value ?? '') }}"
+    {{ ($required ?? false) ? 'required' : '' }}
+    {{ $attributes->merge(['class' => '']) }}
+    autocomplete="off"
+/>
+
+@error($name)
+@include('basecore::components.inputs.partials.error')
+@enderror
