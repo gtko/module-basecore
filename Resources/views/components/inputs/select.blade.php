c897967da277d60dddd6552bed9b4a111aa95904
gtko <gtko@users.noreply.github.com>
2021-12-01 23:43:34 +0100

    Nomics api + command coins et update

--- Resources/views/components/inputs/select.blade.php
+++ Resources/views/components/inputs/select.blade.php
@@ -12,7 +12,7 @@
     id="{{ $name }}"
     name="{{ $name }}"
     {{ ($required ?? false) ? 'required' : '' }}
-    {{ $attributes->merge(['class' => 'block appearance-none w-full text-gray-800 border rounded']) }}
+    {{ $attributes->merge(['class' => 'form-control block appearance-none w-full text-gray-800 border rounded dark:text-white']) }}
     autocomplete="off"
 >{{ $slot }}</select>
 
