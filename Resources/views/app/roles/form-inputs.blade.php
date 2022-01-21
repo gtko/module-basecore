f6add10316e26fed654734458e62918a6cdffe10
gtko <gtko@users.noreply.github.com>
2022-01-21 13:11:37 +0100

    Add seeder permission

--- Resources/views/app/roles/form-inputs.blade.php
+++ Resources/views/app/roles/form-inputs.blade.php
@@ -9,25 +9,27 @@
         ></x-basecore::inputs.text>
     </x-basecore::inputs.group>
 
-    <div class="px-4 my-4">
+    <div class="w-full px-4 my-4">
         <h4 class="font-bold text-lg text-gray-700">
             Assign @lang('crud.permissions.name')
         </h4>
-
-        <div class="grid grid-cols-3 lg:grid-cols-4 gap-4 w-full">
+        <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
             @foreach ($permissions as $permission)
-                <div class="border border-dashed py-2 px-4" x-data="{checked:'{{isset($role) ? $role->hasPermissionTo($permission) : false}}'}"
-                     x-bind:class="{'bg-blue-100' : checked}">
-                <x-basecore::inputs.checkbox
-                    id="permission{{ $permission->id }}"
-                    name="permissions[]"
-                    label="{{ ucfirst($permission->name) }}"
-                    value="{{ $permission->id }}"
-                    :checked="isset($role) ? $role->hasPermissionTo($permission) : false"
-                    :add-hidden-value="false"
-                    x-on:change="checked=!checked"
-                ></x-basecore::inputs.checkbox>
-            </div>
+                <div class="w-full border border-dashed py-2 px-4 bg-blue-100">
+                    <div class="mb-2 font-bold">{{ ucfirst($permission['name']) }}</div>
+                    <div class="w-full grid grid-cols-3 md:grid-cols-2 gap-2 items-center">
+                        @foreach($permission['perms'] as $perms)
+                            <x-basecore::inputs.checkbox
+                                id="permission{{ $perms['model']->id }}"
+                                name="permissions[]"
+                                label="{{ ucfirst($perms['name']) }}"
+                                value="{{ $perms['model']->id }}"
+                                :checked="isset($role) ? $role->hasPermissionTo($perms['model']) : false"
+                                :add-hidden-value="false"
+                            />
+                        @endforeach
+                    </div>
+                </div>
             @endforeach
         </div>
     </div>
