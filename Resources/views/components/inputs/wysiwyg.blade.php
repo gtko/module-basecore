3a96c37c3f8887353daa7bfda3af161e6e43cf68
gtko <gtko@users.noreply.github.com>
2022-02-21 19:29:58 +0100

    WIP

--- Resources/views/components/inputs/wysiwyg.blade.php
+++ Resources/views/components/inputs/wysiwyg.blade.php
@@ -3,60 +3,108 @@
     'label',
     'value',
     'height' => '150',
-    'livewire' => true
+    'livewire' => true,
+    'variableData' => []
 ])
 
 @if($livewire)
+
+    @php
+        $jsonData = [];
+        foreach($variableData as $id => $variable ){
+                $jsonData[] = [
+                    'id' => $id,
+                    'name' =>  $variable['value'],
+                    'title' => '{' . $variable['value'] . '}',
+                    'description' => $variable['description'],
+                ];
+        }
+    @endphp
+
     <div
         x-data="{'content' : $wire.entangle('{{ $name }}').defer}"
         x-init="() => {
-            let updateUser = false;
-            let id = '{{$name}}'.replace('.', '_');
-            $('#'+id).trumbowyg('destroy');
-            $('#'+id).trumbowyg({
-                btns: [['undo', 'redo'], ['bold', 'italic', 'underline', 'strikethrough'], ['link'], ['unorderedList'],],
-                autogrow: true,
-                lang: 'fr',
-            })
-            .on('tbwinit', function(){
-                let mentionify = $(this).closest('.wrapper-mentionify').length > 0;
-                if(mentionify){
-                    let editor = $(this).closest('.trumbowyg-box').find('.trumbowyg-editor');
-                    window.addMentionify((mention) => {
-                        console.log('Add event', editor, mention);
-                        editor[0].addEventListener('focus', (e) => {
-                            e.target.value = e.target.innerText;
-                            console.log('Focus', e);
-                            mention.focus(e)
-                        })
+            console.log('CONTENT', content);
 
-                        editor[0].addEventListener('input', (e) => {
-                            e.target.selectionStart = window.getCaretpos(e.target)
-                            console.log('Caret', e.target.selectionStart, window.getCaretpos(e.target));
-                            e.target.value = e.target.innerText;
-                            console.log('Change', e);
-                            console.log('change editable')
-                        })
+            var PLACEHOLDERS = {{ Illuminate\Support\Js::from($jsonData) }};
+            CKEDITOR.addCss('span > .cke_placeholder { background-color: #ffeec2; }');
+
+            CKEDITOR.replace('{{ $name }}', {
+              plugins: 'autocomplete,textmatch,toolbar,wysiwygarea,basicstyles,link,undo,placeholder',
+              toolbar: [
+                {
+                  name: 'document',
+                  items: ['Undo', 'Redo']
+                },
+                {
+                  name: 'basicstyles',
+                  items: ['Bold', 'Italic']
+                },
+                {
+                  name: 'links',
+                  items: ['Link', 'Unlink']
+                }
+              ],
+              on: {
+                change :  function(evt) {
+                    @this.set('{{$name}}', evt.editor.getData(), true);
+                },
+                instanceReady: (evt) => {
+                   var itemTemplate = `<li data-id='{id}'>
+                    <div><strong class='item-title'>{title}</strong></div>
+                    <div><i>{description}</i></div>
+                    </li>`,
+                   @verbatim
+                        outputTemplate = '{title}<span>&nbsp;</span>';
+                    @endverbatim
+                   var autocomplete = new CKEDITOR.plugins.autocomplete(evt.editor, {
+                        textTestCallback: textTestCallback,
+                        dataCallback: dataCallback,
+                        itemTemplate: itemTemplate,
+                        outputTemplate: outputTemplate
                     });
-                }
-            })
-            .on('tbwchange', function (e) {
-                updateUser = true
-                let value = $(this).trumbowyg('html')
-                console.log('change', value)
-                @this.set('{{$name}}',value , true)
-            });
+                   // Override default getHtmlToInsert to enable rich content output.
+                   autocomplete.getHtmlToInsert = function(item) {
+                      return this.outputTemplate.output(item);
+                   }
+               }
+            },
+            removeButtons: 'PasteFromWord'
+    });
 
+    function textTestCallback(range) {
+        if (!range.collapsed) {
+        return null;
+        }
 
-            $watch('content', (value, oldValue) => {
-                if(!updateUser) {
-                    $('#'+id).trumbowyg('html', value);
-                }
+        return CKEDITOR.plugins.textMatch.match(range, matchCallback);
+    }
 
-                updateUser = false;
-            })
+    function matchCallback(text, offset) {
+        var pattern = /[{]([A-z]|[}])*$/;
+        match = text.slice(0, offset)
+        .match(pattern);
 
-        }"
+        if (!match) {
+            return null;
+        }
+
+        return {
+            start: match.index,
+            end: offset
+        };
+    }
+
+    function dataCallback(matchInfo, callback) {
+        var data = PLACEHOLDERS.filter(function(item) {
+            var itemName = '{' + item.name + '}';
+            return itemName.indexOf(matchInfo.query.toLowerCase()) == 0;
+        });
+
+        callback(data);
+    }
+
+    }"
         wire:ignore
         {{ $attributes->whereDoesntStartWith('wire:model') }}
     >
