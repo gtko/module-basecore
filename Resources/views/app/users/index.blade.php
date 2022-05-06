6bb234c70bdd70be1a6f88013645f6b10cf8bf61
aurelien3333 <aurelienverdeau@gmail.com>
2022-05-06 17:35:25 +0200

    add btn add user

--- Resources/views/app/users/index.blade.php
+++ Resources/views/app/users/index.blade.php
@@ -9,6 +9,7 @@
     </x-slot>
 {{--    <livewire:datalistcrm::data-list :title="'Users'" :type="Modules\BaseCore\DataLists\UserDataList::class"/>--}}
 
+    <a href="/users/create" class="btn btn-primary mt-4" >Ajouter un utilisateur</a>
     <livewire:basecore::list-users />
 
 </x-basecore::app-layout>
