eee852cb3b64f1b9ef81bb8a42636d25ccf2aeac
Soude Jacques <bostersoude@gmail.com>
2022-10-11 13:17:00 +0000

    Add  feature to fetch all contact and email informations

--- Resources/views/components/personne/address-details.blade.php
+++ Resources/views/components/personne/address-details.blade.php
@@ -1,19 +1,33 @@
 <div>
-    <div class="flex flex-col justify-center items-center lg:items-start mt-4">
+    <div class="flex flex-col items-center justify-center mt-4 lg:items-start">
 {{--        ajout de la societe pour yoram crm sur la fiche client--}}
         @if($personne->company ?? false)
-            <div class="truncate sm:whitespace-normal flex items-center mt-1">
+            <div class="flex items-center mt-1 truncate sm:whitespace-normal">
                 @icon('briefcase', null, 'mr-2') {{ $personne->company ?? 'NC'}}
             </div>
         @endif
-        <div class="truncate sm:whitespace-normal flex items-center mt-1">
-            @icon('email', null, 'mr-2') {{ $personne->email ?? "N/C"}}
-        </div>
-        <div class="truncate sm:whitespace-normal flex items-center mt-1">
-            @icon('phone', null, 'mr-2') {{$personne->phone ?? "N/C"}}
-        </div>
+        
+        @forelse ($personne->personne->emails as $email)
+            <div class="flex items-center mt-1 truncate sm:whitespace-normal">
+                @icon('email', null, 'mr-2') {{ $email->email ?? "N/C"}}
+            </div>
+        @empty
+            <div class="flex items-center mt-1 truncate sm:whitespace-normal">
+                @icon('email', null, 'mr-2') {{ $personne->email ?? "N/C"}}
+            </div>
+        @endforelse
+        
+        @forelse ($personne->personne->phones as $phone)
+            <div class="flex items-center mt-1 truncate sm:whitespace-normal">
+                @icon('phone', null, 'mr-2') {{ $phone->phone ?? "N/C"}}
+            </div>
+        @empty
+            <div class="flex items-center mt-1 truncate sm:whitespace-normal">
+                @icon('phone', null, 'mr-2') {{$personne->phone ?? "N/C"}}
+            </div>
+        @endforelse
 
-        <div class="truncate sm:whitespace-normal flex items-start mt-1">
+        <div class="flex items-start mt-1 truncate sm:whitespace-normal">
             @icon('home', null, 'mr-2') {{$personne->fullAddress ?? 'N/C'}}
         </div>
     </div>
